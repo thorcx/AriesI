220c285d338142dd440f0ede21cb2d98e72b5cbd
thorcx <thorcx@163.com>
2017-06-16 11:20:01 +0800

    AriesI Initial Commit

---
+++ Source/AriesI/AriesIGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "AriesIGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARIESI_API AAriesIGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
